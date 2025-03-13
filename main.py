0ff98ff0d55affe821cff718ab18d32c4b96b5fd
Tarkan <142388310+tarkangur@users.noreply.github.com>
2025-03-13 21:13:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+from player import Spaceship
+from alliens import *
+from scoreboard import *
+import time
+
+
+screen = turtle.Screen()
+screen.screensize(canvwidth=1000, canvheight=600)
+
+turtle.mainloop()
