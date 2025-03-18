a6dc733a3aa7eaa745991ef7ec084c42a1b755dd
Tarkan <142388310+tarkangur@users.noreply.github.com>
2025-03-18 22:15:45 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,24 +1,28 @@
 from turtle import Turtle
 
 DISTANCE = 25
-LEFT_LIMIT = -280
-RIGHT_LIMIT = 280
+LEFT_LIMIT = -275
+RIGHT_LIMIT = 275
 
 
 class Spaceship(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
+        #self.screen.addshape("images/spaceship.gif")
+        #self.shape("images/spaceship.gif")
+        self.shape("turtle")
+        self.color("white")
+        self.setheading(90)
         self.goto(0, -270)
-        self.shape("images/spaceship.gif")
 
     def move_left(self):
         if self.xcor() > LEFT_LIMIT:
-            self.backward(DISTANCE)
+            self.goto(self.xcor()-DISTANCE, self.ycor())
 
     def move_right(self):
         if self.xcor() < RIGHT_LIMIT:
-            self.forward(DISTANCE)
+            self.goto(self.xcor()+DISTANCE, self.ycor())
 
     def fire(self):
         pass
