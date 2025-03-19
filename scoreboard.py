ef86a4a070c61d427e27326227d36ce02b589b0a
Tarkan <142388310+tarkangur@users.noreply.github.com>
2025-03-19 21:48:53 +0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,29 @@
 from turtle import Turtle
 
 
-class ScoreBoard(Turtle):
-    def __init__(self, score):
+class Scoreboard(Turtle):
+    def __init__(self):
         super().__init__()
-        self.penup()
         self.color("white")
-        self.goto(200, 350)
-        self.write(score, align="left", font=("Arial", 30, "normal"))
+        self.penup()
         self.hideturtle()
+        self.lives = 3
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-280, 360)
+        self.write(f"Lives: {self.lives}  Score: {self.score}", font=("Arial", 14, "normal"))
+
+    def increase_score(self, points):
+        self.score += points
+        self.update_scoreboard()
+
+    def decrease_life(self):
+        self.lives -= 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(-50, 0)
+        self.write("GAME OVER", font=("Arial", 24, "bold"))
