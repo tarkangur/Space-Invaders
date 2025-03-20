37d1f96432345e133ce0de5377a89c7a578a516f
Tarkan <142388310+tarkangur@users.noreply.github.com>
2025-03-20 16:48:35 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+from bullet import Bullet
 
 DISTANCE = 25
 LEFT_LIMIT = -275
@@ -15,6 +16,7 @@ class Spaceship(Turtle):
         self.color("white")
         self.setheading(90)
         self.goto(0, -270)
+        self.bullets = []
 
     def move_left(self):
         if self.xcor() > LEFT_LIMIT:
@@ -25,5 +27,5 @@ class Spaceship(Turtle):
             self.goto(self.xcor()+DISTANCE, self.ycor())
 
     def fire(self):
-        pass
-
+        new_bullet = Bullet(self.xcor(), self.ycor() + 20, 1)
+        self.bullets.append(new_bullet)
